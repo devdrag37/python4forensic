68fc31a2fd226376bad946f9180d95418b189636
devdrag37 <96565262+devdrag37@users.noreply.github.com>
2022-04-06 17:04:29 +0900

    Update sample1.py

--- 1_4study/Registry/1_basic/sample1.py
+++ 1_4study/Registry/1_basic/sample1.py
@@ -11,10 +11,10 @@ key_console = clsRegf.get_key_by_path(r"Console")
 # You will find class members of key_console by "help(class_variable)"
 
 # Getting a number of VALUE
-print "VALUES #" + str(key_console.get_number_of_values())
+print ("VALUES #" + str(key_console.get_number_of_values()))
 
 # Getting a number of sub-keys
-print "SUBKEY #" + str(key_console.get_number_of_sub_keys())
+print ("SUBKEY #" + str(key_console.get_number_of_sub_keys()))
 
 # Getting value object of index 8
 v0 = key_console.get_value(8)
@@ -22,7 +22,7 @@ v0 = key_console.get_value(8)
 # getting data from value object
 #print "VALUE NAME: " + v0.get_name() + ", DATA: " + v0.get_data()
 
-print "VALUE NAME: " + v0.get_name()
-print "VALUE DATA: " + str(v0.get_data_as_integer())
+print ("VALUE NAME: " + v0.get_name())
+print ("VALUE DATA: " + str(v0.get_data_as_integer()))
 
 clsRegf.close()
